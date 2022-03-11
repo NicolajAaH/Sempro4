2df9240a964ad2a47d32da9e944927ea211f048f
Nicolaj Hansen <nicolaj.it@icloud.com>
2022-03-11 13:52:04 +0100

    -

--- OSGiCore/src/main/java/dk/sdu/mmmi/cbse/Game.java
+++ OSGiCore/src/main/java/dk/sdu/mmmi/cbse/Game.java
@@ -33,9 +33,9 @@ public class Game implements ApplicationListener {
 
     public void init() {
         LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-        cfg.title = "Asteroids";
-        cfg.width = 800;
-        cfg.height = 600;
+        cfg.title = "TowerDefense";
+        cfg.width = 1200;
+        cfg.height = 800;
         cfg.useGL30 = false;
         cfg.resizable = false;
 
