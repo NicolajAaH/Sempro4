b7a5c5f8014e11c4299fa251b0370e18516123f4
Sandra <small11@student.sdu.dk>
2022-04-01 09:49:20 +0200

    Added tile properties

--- OSGiFileHandler/src/main/resources/images/Tiles/maptiles.tsx
+++ OSGiFileHandler/src/main/resources/images/Tiles/maptiles.tsx
@@ -1,4 +1,8 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.2" name="Maptiels" tilewidth="58" tileheight="58" tilecount="4" columns="4">
+<tileset version="1.8" tiledversion="1.8.4" name="Maptiels" tilewidth="58" tileheight="58" tilecount="4" columns="4">
  <image source="maptiles.png" width="232" height="58"/>
+ <tile id="0" type="Path"/>
+ <tile id="1" type="Grass"/>
+ <tile id="2" type="Barrier"/>
+ <tile id="3" type="Tower"/>
 </tileset>
