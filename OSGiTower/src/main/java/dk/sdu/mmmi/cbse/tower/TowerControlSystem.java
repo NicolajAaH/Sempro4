9e94a20e1d7563fffa37c8d5410c66707f2e187c
theislanglands <thlan20@student.sdu.dk>
2022-04-01 09:32:04 +0200

    Tower java files created

---
+++ OSGiTower/src/main/java/dk/sdu/mmmi/cbse/tower/TowerControlSystem.java
@@ -0,0 +1,18 @@
+package dk.sdu.mmmi.cbse.tower;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import dk.sdu.mmmi.cbse.common.data.GameData;
+import dk.sdu.mmmi.cbse.common.data.World;
+import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
+
+public class TowerControlSystem implements IEntityProcessingService {
+    @Override
+    public void process(GameData gameData, World world) {
+
+    }
+
+    @Override
+    public void draw(SpriteBatch batch, World world) {
+
+    }
+}
