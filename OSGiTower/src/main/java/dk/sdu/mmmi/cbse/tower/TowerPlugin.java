e47ff8bde0b9918d10f561bdb5165e619dc47339
theislanglands <thlan20@student.sdu.dk>
2022-04-01 09:32:04 +0200

    Tower java files created

---
+++ OSGiTower/src/main/java/dk/sdu/mmmi/cbse/tower/TowerPlugin.java
@@ -0,0 +1,26 @@
+package dk.sdu.mmmi.cbse.tower;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import dk.sdu.mmmi.cbse.common.data.Entity;
+import dk.sdu.mmmi.cbse.common.data.GameData;
+import dk.sdu.mmmi.cbse.common.data.World;
+import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
+
+public class TowerPlugin implements IGamePluginService {
+
+    @Override
+    public void start(GameData gameData, World world) {
+        
+    }
+
+    @Override
+    public void stop(GameData gameData, World world) {
+
+    }
+
+    @Override
+    public Entity create(SpriteBatch batch, GameData gameData, World world, Texture texture) {
+        return null;
+    }
+}
