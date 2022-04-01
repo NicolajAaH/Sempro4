4734cbb6926b0913dba7c1114654432f2b994ba3
theislanglands <thlan20@student.sdu.dk>
2022-04-01 09:53:37 +0200

    Implemented TowerSPI to TowerControl

---
+++ OSGiCommonTower/src/main/java/dk/sdu/mmmi/cbse/commontower/Tower.java
@@ -0,0 +1,34 @@
+package dk.sdu.mmmi.cbse.commontower;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import dk.sdu.mmmi.cbse.common.data.Entity;
+import dk.sdu.mmmi.cbse.common.data.entityparts.PositionPart;
+
+public class Tower extends Entity {
+
+    public Tower(Sprite sprite) {
+        super(sprite);
+    }
+
+    @Override
+    public void draw(Batch batch) {
+        batch.begin();
+        update(Gdx.graphics.getDeltaTime());
+        super.draw(batch);
+        batch.end();
+    }
+
+    public void update(float delta){
+        PositionPart positionPart = this.getPart(PositionPart.class);
+        this.setPosition(positionPart.getX(), positionPart.getY());
+        /*
+        if (positionPart.getLastChange() == "Up") this.setRotation(0);
+        if (positionPart.getLastChange() == "Down") this.setRotation(180);
+        if (positionPart.getLastChange() == "Left") this.setRotation(90);
+        if (positionPart.getLastChange() == "Right") this.setRotation(270);
+         */
+    }
+}
