4fdd6bd0b613f857d92c55db27079aea1fc0a1c9
Nicolaj Hansen <nicolaj.it@icloud.com>
2022-04-01 09:19:35 +0200

    Refactoring of map into Common

--- OSGiCore/src/main/java/dk/sdu/mmmi/cbse/Game.java
+++ OSGiCore/src/main/java/dk/sdu/mmmi/cbse/Game.java
@@ -55,6 +55,7 @@ public class Game implements ApplicationListener {
 
     @Override
     public void create() {
+        world.setTiledMap(new TmxMapLoader().load("Map.tmx"));
 
         renderer = new OrthogonalTiledMapRenderer(world.getMap());
         batch = new SpriteBatch();
