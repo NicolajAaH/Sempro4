c4d2acc045f949a714029ca44f1f1f6e1bcc5d1c
Nicolaj Hansen <nicolaj.it@icloud.com>
2022-04-08 12:21:28 +0200

    Created map components

---
+++ OSGiCommonMap/src/main/java/dk/sdu/mmmi/cbse/commonmap/IMap.java
@@ -0,0 +1,5 @@
+package dk.sdu.mmmi.cbse.commonmap;
+
+public interface IMap {
+
+}
---
+++ OSGiMap/src/main/java/dk/sdu/mmmi/cbse/map/MapPlugin.java
@@ -0,0 +1,29 @@
+package dk.sdu.mmmi.cbse.map;
+
+import dk.sdu.mmmi.cbse.common.data.Entity;
+import dk.sdu.mmmi.cbse.common.data.GameData;
+import dk.sdu.mmmi.cbse.common.data.Types;
+import dk.sdu.mmmi.cbse.common.data.World;
+import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
+
+public class MapPlugin implements IGamePluginService{
+    @Override
+    public void start(GameData gameData, World world) {
+
+    }
+
+    @Override
+    public void stop(GameData gameData, World world) {
+
+    }
+
+    @Override
+    public Entity create(GameData gameData, World world) {
+        return null;
+    }
+
+    @Override
+    public Types getType() {
+        return null;
+    }
+}
