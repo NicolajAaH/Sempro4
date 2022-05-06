27152af5fa558936d06d4e3b14934a409c7fead4
Nicolaj Hansen <nicolaj.it@icloud.com>
2022-05-06 09:48:57 +0200

    Fix of merge

--- OSGiCollision/src/main/java/dk/sdu/mmmi/cbse/collision/CollisionManager.java
+++ OSGiCollision/src/main/java/dk/sdu/mmmi/cbse/collision/CollisionManager.java
@@ -9,52 +9,42 @@ import dk.sdu.mmmi.cbse.common.data.entityparts.PositionPart;
 import dk.sdu.mmmi.cbse.common.services.IPostEntityProcessingService;
 
 import java.util.ArrayList;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadPoolExecutor;
 
 public class CollisionManager implements IPostEntityProcessingService {
     @Override
     public void process(GameData gameData, World world) {
         ArrayList<Entity> entities = new ArrayList<>(world.getEntities());
 
-        ThreadPoolExecutor executor =
-                (ThreadPoolExecutor) Executors.newFixedThreadPool(3);
 
+        for (Entity iEntity: entities) {
+            for (Entity oEntity : entities) {
 
-        for (int i = 0; i < entities.size(); i++) {
-            final int x = i;
+                if (iEntity.getType() == Types.TOWER || oEntity.getType() == Types.TOWER)
+                    continue;
 
-                for (int o = x + 1; o < entities.size(); o++) {
+                if (iEntity.getType() == oEntity.getType()) //Should not collide when equal type
+                    continue;
 
+                if ((iEntity.getType() == Types.PLAYER || oEntity.getType() == Types.PLAYER) && (iEntity.getType() == Types.PROJECTILE || oEntity.getType() == Types.PROJECTILE))
+                    continue; //Player and projectile should not collide
 
-                    // Get the entities
-                    Entity iEntity = entities.get(x);
-                    Entity oEntity = entities.get(o);
+                // Get the position part for the entities
+                PositionPart iPosition = iEntity.getPart(PositionPart.class);
+                PositionPart oPosition = oEntity.getPart(PositionPart.class);
 
-                    if (iEntity.getType() == Types.TOWER || oEntity.getType() == Types.TOWER)
-                        continue;
-
-                    if (iEntity.getType() == oEntity.getType()) //Should not collide when equal type
-                        continue;
-
-                    if ((iEntity.getType() == Types.PLAYER || oEntity.getType() == Types.PLAYER) && (iEntity.getType() == Types.PROJECTILE || oEntity.getType() == Types.PROJECTILE))
-                        continue; //Player and projectile should not collide
-
-                    // Get the position part for the entities
-                    PositionPart iPosition = iEntity.getPart(PositionPart.class);
-                    PositionPart oPosition = oEntity.getPart(PositionPart.class);
+                // Calculate distance between two entities
+                double distance = Math.sqrt(Math.pow((iPosition.getX() - oPosition.getX()), 2) + Math.pow((iPosition.getY() - oPosition.getY()), 2));
 
                 if (distance < (iEntity.getRadius() + oEntity.getRadius())) {
 
                     LifePart iLifePart = iEntity.getPart(LifePart.class);
-
-                    if(iEntity.getType() == Types.PROJECTILE) world.removeEntity(iEntity);
-
-                    iLifePart.setLife(iLifePart.getLife()-1);
+                    if (iEntity.getType() == Types.PROJECTILE)
+                        world.removeEntity(iEntity);
+                    iLifePart.setLife(iLifePart.getLife() - 1);
                 }
 
+            }
 
         }
-
     }
 }
